1f23f2f5d29d729c50d5a88caf3914bcf35084e4
Sohelkhan <sohelpathan121101@gmail.com>
2024-05-26 10:42:54 +0530

    First Commit

---
+++ src/app.py
@@ -0,0 +1,32 @@
+# #!/usr/bin/env python
+# import flask
+
+# app = flask.Flask(__name__)
+
+# @app.route('/')
+# @app.route('/hello/')
+# def hello_world():
+#     return 'Hello World!\n'
+
+# @app.route('/hello/<username>') # dynamic route
+# def hello_user(username):
+#     return 'I have been Updated my friend %s!\n' % username
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')     # open for everyone
+#!/usr/bin/env python
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!\n'
+
+@app.route('/hello/<username>') # dynamic route
+def hello_user(username):
+    return 'I have been Updated my friend %s!\n' % username
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')     # open for everyone
